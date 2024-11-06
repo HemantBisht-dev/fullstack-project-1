d5757044029cddc6a890ff42e257b19869fc60f5
Hemant Bisht <hmntbst2210@gmail.com>
2024-11-06 14:37:06 +0530

    added post api

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,41 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/productmodel.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT||3000
+// middleware - it runs before the send the response back to client
+app.use(express.json()); // allows us to accept json data in the req.body
 
-app.listen(PORT,()=>{
+app.post("/api/product", async(req,res) => {
+  const product = req.body; // user will send this data
+
+  // if user didn't provide fields
+  if (!product.name || !product.price || !product.image) {
+    // 400 - server cannot process the request due to client error request
+    return res
+      .status(400)
+      .json({ success: false, message: "please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    // 201 - request succeeded and new resource was created (POST, PUT)
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("error in created product: ", error.message);
+    // 500 - internal server error
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   connectDB();
-  console.log('server started at http://localhost:3000');
-})
+  console.log("server started at http://localhost:" + PORT);
+});
